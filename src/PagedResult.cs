dd7bc1b7b534526c0c441ef555c4422065164e01
Alex Brown <alex@cohoda.com>
2021-10-31 21:05:15 +0000

    added PagedResult

---
+++ src/PagedResult.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GardeningExpress.DespatchCloudClient
+{
+    public class PagedResult<T>
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("per_page")]
+        public string PerPage { get; set; }
+
+        [JsonProperty("current_page")]
+        public int CurrentPage { get; set; }
+
+        [JsonProperty("last_page")]
+        public int LastPage { get; set; }
+
+        [JsonProperty("next_page_url")]
+        public string NextPageUrl { get; set; }
+
+        [JsonProperty("prev_page_url")]
+        public object PreviousPageUrl { get; set; }
+
+        [JsonProperty("from")]
+        public int From { get; set; }
+
+        [JsonProperty("to")]
+        public int To { get; set; }
+
+        [JsonProperty("data")]
+        public List<T> Data { get; set; }
+    }
+}
