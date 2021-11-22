2d754863196d446489b8b052f17fd3d8434d365d
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2021-11-22 11:16:27 +0000

    update usings

--- tests/GardeningExpress.DespatchCloudClient.Tests.Unit/Orders/DespatchCloudSearchOrdersTests.cs
+++ tests/GardeningExpress.DespatchCloudClient.Tests.Unit/Orders/DespatchCloudSearchOrdersTests.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using GardeningExpress.DespatchCloudClient.DTO;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Moq.Contrib.HttpClient;
