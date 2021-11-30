cf4b925589df6a245e2392886a79b099ceb90d6f
Alex Brown <alex@cohoda.com>
2021-11-30 23:04:37 +0000

    added InventoryUpdateRequest

---
+++ src/DTO/InventoryUpdateRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GardeningExpress.DespatchCloudClient.DTO
+{
+    public class InventoryUpdateRequest
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("sku")]
+        public string SKU { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("custom_fields")]
+        public Dictionary<string, object> CustomFields { get; set; }
+    }
+}
