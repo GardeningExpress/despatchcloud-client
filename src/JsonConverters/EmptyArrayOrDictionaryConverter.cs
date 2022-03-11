43124fc8add3f570b19ed184c2444f48a52c4c57
Alex Brown <alex@cohoda.com>
2022-03-11 15:34:56 +0000

    initial fix for handling different custom_fields types

--- src/JsonConverters/EmptyArrayOrDictionaryConverter.cs
+++ src/JsonConverters/EmptyArrayOrDictionaryConverter.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json.Linq;
 
 namespace GardeningExpress.DespatchCloudClient.JsonConverters
 {
-    internal class EmptyArrayOrDictionaryConverter : JsonConverter
+    public class EmptyArrayOrDictionaryConverter : JsonConverter
     {
         public override bool CanConvert(Type objectType)
         {
@@ -15,12 +15,31 @@ namespace GardeningExpress.DespatchCloudClient.JsonConverters
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var token = JToken.Load(reader);
+
             return token.Type switch
             {
                 JTokenType.Object => token.ToObject(objectType, serializer),
-                JTokenType.Array when !token.HasValues => Activator.CreateInstance(objectType),
+                JTokenType.Array when !token.HasValues => new Dictionary<string, object>(),
+                JTokenType.Array when token.HasValues => Go(token, serializer),
+                JTokenType.Null => new Dictionary<string, object>(),
                 _ => throw new JsonSerializationException("Object or empty array expected")
             };
+        }
+
+        private Dictionary<string, object> Go(JToken token, JsonSerializer serializer)
+        {
+            var result = new Dictionary<string, object>();
+
+            if (token is JArray jArray)
+                foreach (var jToken in jArray.Children<JToken>())
+                {
+                    foreach (var (key, value) in jToken.ToObject<Dictionary<string, object>>())
+                    {
+                        result.Add(key, value);
+                    }
+                }
+
+            return result;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
---
+++ tests/GardeningExpress.DespatchCloudClient.Tests.Unit/InventoryDeserializationTests.cs
@@ -0,0 +1,71 @@
+﻿using GardeningExpress.DespatchCloudClient.DTO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Shouldly;
+
+namespace GardeningExpress.DespatchCloudClient.Tests.Unit
+{
+    public class InventoryDeserializationTests
+    {
+        [Test]
+        public void Deserializes_empty_array_custom_fields()
+        {
+            var json = "{\"custom_fields\": []}";
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.ShouldBeEmpty();
+        }
+        
+        [Test]
+        public void Deserializes_null_custom_fields()
+        {
+            var json = "{\"custom_fields\": null}";
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.ShouldBeEmpty();
+        }
+        
+        [Test]
+        public void Deserializes_object_custom_fields_with_one_item()
+        {
+            var json = "{\"custom_fields\": {\"pot-size-3\": \"6 Litre\"}}";
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.Count.ShouldBe(1);
+            result.CustomFields.ShouldContainKeyAndValue("pot-size-3", "6 Litre");
+        }
+        
+        [Test]
+        public void Deserializes_array_custom_fields_with_one_item()
+        {
+            var json = "{\"custom_fields\": [{\"pot-size-3\": \"6 Litre\"}]}";
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.Count.ShouldBe(1);
+            result.CustomFields.ShouldContainKeyAndValue("pot-size-3", "6 Litre");
+        }
+
+        [Test]
+        public void Deserializes_object_custom_fields_with_two_items()
+        {
+            var json = "{\"custom_fields\":{\"pot-size-3\":\"6 Litre\",\"passport-2\":\"test\"}}";
+            
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.Count.ShouldBe(2);
+            result.CustomFields.ShouldContainKeyAndValue("pot-size-3", "6 Litre");
+            result.CustomFields.ShouldContainKeyAndValue("passport-2", "test");
+        }
+
+        [Test]
+        public void Deserializes_array_custom_fields_with_two_items()
+        {
+            var json = "{\"custom_fields\":[{\"pot-size-3\":\"6 Litre\"},{\"passport-2\":\"test\"}]}";
+            var result = JsonConvert.DeserializeObject<Inventory>(json);
+            
+            result.CustomFields.Count.ShouldBe(2);
+            result.CustomFields.ShouldContainKeyAndValue("pot-size-3", "6 Litre");
+            result.CustomFields.ShouldContainKeyAndValue("passport-2", "test");
+        }
+    }
+}
