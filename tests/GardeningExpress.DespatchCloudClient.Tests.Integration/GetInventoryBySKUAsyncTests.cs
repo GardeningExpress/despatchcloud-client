145d89859b824eb9115a021e7e98764e868f86de
Alex Brown <alex@cohoda.com>
2023-10-19 21:44:40 +0100

    fix unit test

--- tests/GardeningExpress.DespatchCloudClient.Tests.Integration/GetInventoryBySKUAsyncTests.cs
+++ tests/GardeningExpress.DespatchCloudClient.Tests.Integration/GetInventoryBySKUAsyncTests.cs
@@ -23,19 +23,44 @@ namespace GardeningExpress.DespatchCloudClient.Tests.Integration
         }
 
         [Test]
-        public async Task Returns_Inventory_For_SKU_With_Malformed_CustomFields()
+        public async Task Returns_Inventory_For_SKU_With_CustomFields()
         {
             var result = await DespatchCloudHttpClient
-                .GetInventoryBySKUAsync("PP23235");
+                .GetInventoryBySKUAsync("DEAL16071");
 
             result.ShouldNotBeNull();
             result.Data.ShouldNotBeNull();
             result.Data.Name.ShouldNotBeNullOrEmpty();
-            result.Data.Name.ShouldStartWith("Bellis Hot Pink");
-            
+            result.Data.Name.ShouldStartWith("Prunus triloba");
+            result.Data.CustomFields.ShouldContainKeyAndValue("pot-size-3", "2-3 Litre");
+        }
+        
+        [Test]
+        public async Task Returns_Inventory_For_SKU_With_Empty_CustomFields()
+        {
+            var result = await DespatchCloudHttpClient
+                .GetInventoryBySKUAsync("P800927");
+
+            result.ShouldNotBeNull();
+            result.Data.ShouldNotBeNull();
+            result.Data.Name.ShouldNotBeNullOrEmpty();
+            result.Data.Name.ShouldStartWith("Sempervivum Houseleeks");
             result.Data.CustomFields.ShouldBeEmpty();
         }
+            
+        [Test]
+        //throwing exception 25th march 2022
+        public async Task Returns_Inventory_For_SKU_S21041()
+        {
+            var result = await DespatchCloudHttpClient
+                .GetInventoryBySKUAsync("S21041");
 
+            result.ShouldNotBeNull();
+            result.Data.ShouldNotBeNull();
+            result.Data.Name.ShouldNotBeNullOrEmpty();
+            result.Data.Name.ShouldStartWith("Syringa microphylla");
+        }
+        
         [Test]
         public async Task Returns_Null_For_SKU_That_Does_Not_Exist()
         {
