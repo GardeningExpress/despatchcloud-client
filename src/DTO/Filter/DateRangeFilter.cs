31a8f23694521cf5e777ca6483ed11b12ccf025a
Alex Brown <alex@cohoda.com>
2023-11-21 21:24:35 +0000

    tidy

--- src/DTO/Filter/DateRangeFilter.cs
+++ src/DTO/Filter/DateRangeFilter.cs
@@ -7,13 +7,13 @@ namespace GardeningExpress.DespatchCloudClient.DTO.Filter
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
-        internal long StartTimestamp { get { return ConvertToTimestamp(StartDate); } }
-        internal long EndTimestamp { get { return ConvertToTimestamp(EndDate); } }
+        internal long StartTimestamp => ConvertToTimestamp(StartDate);
+        internal long EndTimestamp => ConvertToTimestamp(EndDate);
 
         private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         private static long ConvertToTimestamp(DateTime value)
         {
-            TimeSpan elapsedTime = value - Epoch;
+            var elapsedTime = value - Epoch;
             return (long)elapsedTime.TotalSeconds;
         }
     }
