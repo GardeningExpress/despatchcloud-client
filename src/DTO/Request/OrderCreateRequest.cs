61502f2762a38ebbf424c1267e151ccca75a9cb9
Mohit Sengar <mohitsingh.shiv@gmail.com>
2023-12-07 06:50:40 +0000

    fix: added default values for Order model calss

--- src/DTO/Request/OrderCreateRequest.cs
+++ src/DTO/Request/OrderCreateRequest.cs
@@ -6,116 +6,116 @@ namespace GardeningExpress.DespatchCloudClient.DTO.Request
     public class OrderCreateRequest
     {
         [JsonProperty("status_id")]
-        public int StatusId { get; set; }
+        public int StatusId { get; set; } = 0;
 
         [JsonProperty("date_received")]
-        public string DateReceived { get; set; }
+        public string DateReceived { get; set; } = string.Empty;
 
         [JsonProperty("shipping_method_requested")]
-        public string ShippingMethodRequested { get; set; }
+        public string ShippingMethodRequested { get; set; } = string.Empty;
 
         [JsonProperty("one_off_shipment")]
-        public string OneOffShipment { get; set; }
+        public string OneOffShipment { get; set; } = string.Empty;
 
         [JsonProperty("channel_alt_id")]
-        public string ChannelAltId { get; set; }
+        public string ChannelAltId { get; set; } = string.Empty;
 
         [JsonProperty("payment_method")]
-        public string PaymentMethod { get; set; }
+        public string PaymentMethod { get; set; } = string.Empty;
 
         [JsonProperty("payment_ref")]
-        public string PaymentRef { get; set; }
+        public string PaymentRef { get; set; } = string.Empty;
 
         [JsonProperty("payment_currency")]
-        public string PaymentCurrency { get; set; }
+        public string PaymentCurrency { get; set; } = string.Empty;
 
         [JsonProperty("total_paid")]
-        public string TotalPaid { get; set; }
+        public string TotalPaid { get; set; } = string.Empty;
 
         [JsonProperty("total_discount")]
-        public string TotalDiscount { get; set; }
+        public string TotalDiscount { get; set; } = string.Empty;
 
         [JsonProperty("total_tax")]
-        public string TotalTax { get; set; }
+        public string TotalTax { get; set; } = string.Empty;
 
         [JsonProperty("total_weight")]
-        public string TotalWeight { get; set; }
+        public string TotalWeight { get; set; } = string.Empty;
 
         [JsonProperty("email")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [JsonProperty("phone_one")]
-        public string PhoneOne { get; set; }
+        public string PhoneOne { get; set; } = string.Empty;
 
         [JsonProperty("contact_id")]
-        public string ContactId { get; set; }
+        public string ContactId { get; set; } = string.Empty;
 
         [JsonProperty("vat_number")]
-        public string VatNumber { get; set; }
+        public string VatNumber { get; set; } = string.Empty;
 
         [JsonProperty("eori_number")]
-        public string EoriNumber { get; set; }
+        public string EoriNumber { get; set; } = string.Empty;
 
         [JsonProperty("tax_id")]
-        public string TaxId { get; set; }
+        public string TaxId { get; set; } = string.Empty;
 
         [JsonProperty("shipping_name")]
-        public string ShippingName { get; set; }
+        public string ShippingName { get; set; } = string.Empty;
 
         [JsonProperty("shipping_name_company")]
-        public string ShippingNameCompany { get; set; }
+        public string ShippingNameCompany { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_line_one")]
-        public string ShippingAddressLineOne { get; set; }
+        public string ShippingAddressLineOne { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_line_two")]
-        public string ShippingAddressLineTwo { get; set; }
+        public string ShippingAddressLineTwo { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_city")]
-        public string ShippingAddressCity { get; set; }
+        public string ShippingAddressCity { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_county")]
-        public string ShippingAddressCounty { get; set; }
+        public string ShippingAddressCounty { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_country")]
-        public string ShippingAddressCountry { get; set; }
+        public string ShippingAddressCountry { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_postcode")]
-        public string ShippingAddressPostcode { get; set; }
+        public string ShippingAddressPostcode { get; set; } = string.Empty;
 
         [JsonProperty("shipping_address_iso")]
-        public string ShippingAddressIso { get; set; }
+        public string ShippingAddressIso { get; set; } = string.Empty;
 
         [JsonProperty("invoice_name")]
-        public string InvoiceName { get; set; }
+        public string InvoiceName { get; set; } = string.Empty;
 
         [JsonProperty("invoice_name_company")]
-        public string InvoiceNameCompany { get; set; }
+        public string InvoiceNameCompany { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_line_one")]
-        public string InvoiceAddressLineOne { get; set; }
+        public string InvoiceAddressLineOne { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_line_two")]
-        public string InvoiceAddressLineTwo { get; set; }
+        public string InvoiceAddressLineTwo { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_city")]
-        public string InvoiceAddressCity { get; set; }
+        public string InvoiceAddressCity { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_county")]
-        public string InvoiceAddressCounty { get; set; }
+        public string InvoiceAddressCounty { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_country")]
-        public string InvoiceAddressCountry { get; set; }
+        public string InvoiceAddressCountry { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_postcode")]
-        public string InvoiceAddressPostcode { get; set; }
+        public string InvoiceAddressPostcode { get; set; } = string.Empty;
 
         [JsonProperty("invoice_address_iso")]
-        public string InvoiceAddressIso { get; set; }
+        public string InvoiceAddressIso { get; set; } = string.Empty;
 
 
         [JsonProperty("customer_comments")]
-        public string CustomerComments { get; set; }
+        public string CustomerComments { get; set; } = string.Empty;
 
         [JsonProperty("notes")]
         public List<string> Notes { get; set; }
@@ -127,7 +127,7 @@ namespace GardeningExpress.DespatchCloudClient.DTO.Request
         public int? FulfilmentClientId { get; set; }
 
         [JsonProperty("shipping_paid")]
-        public string ShippingPaid { get; set; }
+        public string ShippingPaid { get; set; } = string.Empty;
 
         [JsonProperty("manual_channel_id")]
         public int ManualChannelId { get; set; }
