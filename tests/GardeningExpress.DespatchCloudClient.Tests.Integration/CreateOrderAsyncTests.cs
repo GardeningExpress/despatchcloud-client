ea1f5907e5607c3b6c13fe0b6d8a53a85a26d5e2
Chad P <chaddotnet@gmail.com>
2024-03-13 16:01:15 +0700

    remove gogroopie mappers and references

--- tests/GardeningExpress.DespatchCloudClient.Tests.Integration/CreateOrderAsyncTests.cs
+++ tests/GardeningExpress.DespatchCloudClient.Tests.Integration/CreateOrderAsyncTests.cs
@@ -1,7 +1,5 @@
 ﻿using GardeningExpress.DespatchCloudClient.DTO.Request;
-using GardeningExpress.DespatchCloudClient.Model.GoGroopie;
 using GardeningExpress.DespatchCloudClient.Tests.Integration.Utils;
-using Newtonsoft.Json;
 using NUnit.Framework;
 using System.Threading.Tasks;
 
@@ -12,8 +10,6 @@ namespace GardeningExpress.DespatchCloudClient.Tests.Integration
         protected override Task MethodForAuthTest() =>
             DespatchCloudHttpClient
                 .CreateOrderAsync(new OrderCreateRequest());
-
-        private readonly string goGroopieProductJson = "{ \"platform\": \"GoGroopie.com\", \"deal_id\": \"11111\", \"product\": \"Test deal 4 Colours\", \"voucher_code\": \"1112223334\", \"redeem_date\": \"19-10-2019\", \"order_id\": \"1111111111\", \"price_options\": \"Coffee\", \"price\": \"10.99\", \"currency\": \"GBP\", \"full_name\": \"John Doe\", \"email\": \"john.doe@domain.com\", \"phone\": \"+333331111111\", \"house\": \"11\", \"street\": \"test street\", \"city\": \"London\", \"postcode\": \"SE15LB\", \"country_code\": \"GB\", \"sku\": \"01-0111\", \"pipe_deal_id\": \"11055\", \"postage_price\": \"2.99\", \"net_merchant_return\": \"5.99\"  }";
 
         private OrderCreateRequest newOrder;
 
@@ -53,21 +49,5 @@ namespace GardeningExpress.DespatchCloudClient.Tests.Integration
             Assert.AreEqual(result.Error, $"Error executing statement: Column '{jsonField}' cannot be null");
         }
 
-        [Test]
-        public async Task CreateOrderAsync_WithGoGroopieProduct_ShouldReturnANewOrderResponse()
-        {
-            // ARRANGE
-            var product = JsonConvert.DeserializeObject<Product>(goGroopieProductJson);
-            var order = Helpers.ThirdPartyOrderHelper.ConvertGoGroopieProductToOrderRequest(product);
-            order.InvoiceName = "Integration Tests: CreateOrderAsync()";
-
-            // ACT
-            var result = await DespatchCloudHttpClient.CreateOrderAsync(order);
-
-            // ASSERT
-            Assert.IsTrue(result.IsSuccess);
-            Assert.NotNull(result.Data);
-            Assert.NotNull(result.Data.Id); // Verify trackable id set
-        }
     }
 }
